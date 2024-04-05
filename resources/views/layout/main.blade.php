ed6d881bc8081f7b540af74de79c3b2dba14b7af
Fahmii19 <fhmialfi79@gmail.com>
2024-04-05 15:39:02 +0700

    Update tab activation in main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10755,7 +10755,7 @@ Bintang 3 ke atas', 'Gedung Peribadatan', 'Perpustakaan', 'Bar', 'Perkumpulan So
                 });
             });
 
-            activateTab("btn-6");
+            activateTab("btn-1");
         });
 
     </script>
